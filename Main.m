f64cd69d10bf393b41b047215b1af8b9a386c758
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-01 13:55:03 -0800

    [Question 5] Subsampled images are same dimensions, 'removed' by equating Cb/Cr components to 128.  Still subject to change.  Emailed Kumar

--- Main.m
+++ Main.m
@@ -11,6 +11,7 @@ rgbImage = imread('Flooded_house.jpg','jpg');            %Changed path so that
                                                          %give me erros
                                                          %when running the
                                                          %script
+
 rgbImage(:,:,1); % red component
 % disp('^ Red Component');
 rgbImage(:,:,2); % idk component
@@ -80,36 +81,47 @@ Y = 1; Cb = 2; Cr = 3;
 % use a for loop to access the cr and cb components and then imshow to
 % Does the index start at 0?
 
+% Setting an ycbcr var to work with 
+ycbcrSubsampled=rgb2ycbcr(rgbImage);
 
-% for r = 1:rows
-%     for c = 1:columns
-%         % for every row, go through every column
-%         % if the row number is even OR the column is even, 
-%         % zero out all Cb and Cr for that index
-%         
-%         if (mod(r, 2) == 0) || (mod(c, 2) == 0) % I hope this logic makes sense
-%             ycbcr(r, c, Cb) = 0;
-%             ycbcr(r, c, Cr) = 0;
-%         end
-%     end
-% end
+
+for r = 1:rows
+    for c = 1:columns
+        % for every row, go through every column
+        % if the row number is even OR the column is even, 
+        % zero out all Cb and Cr for that index
+        
+        if (mod(r, 2) == 0) || (mod(c, 2) == 0) % I hope this logic makes sense
+            ycbcrSubsampled(r, c, Cb) = 128;
+            ycbcrSubsampled(r, c, Cr) = 128;
+            % I put 128 because when I orignally put 0, ycbcrSubsampled
+            % had 128 values and zeroes for the rest
+            % the image looks closer to the original image
+        end
+    end
+end
+
 
 %I dont think we are supposed to zero out all even rows and columns,
 %I understood that we have to remove them.
+%I think I removed it by equating it to 128
 
 
 %Copying all odd rows and columns from ycbcr into matrix ycbcrSubsampled
 
-ycbcrSubsampled(:,:,Cb:Cr) = ycbcr(1:2:end,1:2:end,Cb:Cr);  
+% ycbcrSubsampled(:,:,Cb:Cr) = ycbcr(1:2:end,1:2:end,Cb:Cr);  
                                                
 luma(:,:,Y) = ycbcr(:,:,Y);        %After doing this the subsampled
                                    %images look like the original ones
                                    %but smaller
+                                       
 
-                                               
-% TODO this does not look like the sample outputs
+% Display
 % figure, imshow(ycbcrSubsampled(:,:,Cb)); title('ycbcr: Cb Subsampled');
 % figure, imshow(ycbcrSubsampled(:,:,Cr)); title('ycbcr: Cr Subsampled');
+% ycbcrSubsampled(:,:,Y)
+% ycbcrSubsampled(:,:,Cb)
+% ycbcrSubsampled(:,:,Cr)
 
 %% 6. Upsample and display the Cb and Cr bands using: (15 points) %%
 
@@ -122,7 +134,6 @@ luma(:,:,Y) = ycbcr(:,:,Y);        %After doing this the subsampled
 % pixels in its top and bottom sides.
 
 % [SubsampledRow, SubsampledColumn, SubsampledChannels] = size(ycbcrSubsampled);
-
 ycbcrReconstructed = luma(:,:,Y);
 
 % rCount = 1;
@@ -134,6 +145,7 @@ ycbcrReconstructed = luma(:,:,Y);
 %work in progress
 
 ycbcrReconstructed(1:2:end,1:2:end,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
+% ycbcrReconstructed(:,:,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
 
 oddPixelRows = (ycbcrSubsampled(:,1:1:end-1,Cb:Cr) + ycbcrSubsampled(:,2:1:end,Cb:Cr))/2;
 
@@ -148,6 +160,7 @@ evenPixelRows = (ycbcrSubsampled(1:1:end-1,:,Cb:Cr) + ycbcrSubsampled(2:1:end,:,
 % end
 
 ycbcrReconstructed(1:2:end,2:2:end-1,Cb:Cr) = oddPixelRows(:,:,Cb-1:Cr-1);
+% ycbcrReconstructed(:,:,Cb:Cr) = oddPixelRows(:,:,Cb:Cr);
 ycbcrReconstructed(2:2:end-1,:,Cb:Cr) = (ycbcrReconstructed(1:2:end-2,:,Cb:Cr) + ycbcrReconstructed(3:2:end,:,Cb:Cr))/2;
 
 
@@ -184,3 +197,9 @@ ycbcrReconstructed(2:2:end-1,:,Cb:Cr) = (ycbcrReconstructed(1:2:end-2,:,Cb:Cr) +
 figure, imshow(ycbcrReconstructed); title('Reconstructed'); %Just to see what Im getting
 
 figure, imshow(ycbcr); title('Original'); %Original
+
+% 6.2. Simple row or column replication. %
+
+
+
+%% 7. Convert the image into RGB format. (5 points) %%
