97a2912652e8650619dd66959e49e450160ed8ff
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-01 14:31:42 -0800

    [Question 6.2] Replicated rows and columns

--- Main.m
+++ Main.m
@@ -125,81 +125,103 @@ luma(:,:,Y) = ycbcr(:,:,Y);        %After doing this the subsampled
 
 %% 6. Upsample and display the Cb and Cr bands using: (15 points) %%
 
-% 6.1. Linear interpolation %
-
-% For odd row, compute the missing pixel as an average of the
-% pixels in its right and left sides.
-
-% For even rows, compute the missing pixel as an average of the
-% pixels in its top and bottom sides.
-
-% [SubsampledRow, SubsampledColumn, SubsampledChannels] = size(ycbcrSubsampled);
-ycbcrReconstructed = luma(:,:,Y);
-
-% rCount = 1;
-% cCount = 1;
-
-%Copying all pixels from ycbcrSubsampled into ycbcrReconstructed at every
-%odd location
-
-%work in progress
-
-ycbcrReconstructed(1:2:end,1:2:end,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
-% ycbcrReconstructed(:,:,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
-
-oddPixelRows = (ycbcrSubsampled(:,1:1:end-1,Cb:Cr) + ycbcrSubsampled(:,2:1:end,Cb:Cr))/2;
-
-% if(mod(columns,2) == 0)
-%     oddPixelRows(:,end,Cb-1:Cr-1) = oddPixelRows(:,end-1,Cb-1:Cr-1);
-% end
-
-evenPixelRows = (ycbcrSubsampled(1:1:end-1,:,Cb:Cr) + ycbcrSubsampled(2:1:end,:,Cb:Cr))/2;
-
-% if(mod(rows,2) == 0)
-%     evenPixelRows (end,:,Cb-1:Cr-1) = evenPixelRows (end-1,:,Cb-1:Cr-1);
-% end
-
-ycbcrReconstructed(1:2:end,2:2:end-1,Cb:Cr) = oddPixelRows(:,:,Cb-1:Cr-1);
-% ycbcrReconstructed(:,:,Cb:Cr) = oddPixelRows(:,:,Cb:Cr);
-ycbcrReconstructed(2:2:end-1,:,Cb:Cr) = (ycbcrReconstructed(1:2:end-2,:,Cb:Cr) + ycbcrReconstructed(3:2:end,:,Cb:Cr))/2;
-
-
-
-% for r=1:rows
-%     for c=1:columns          %this works but Im working on something simpler above
-%         if(mod(c,2) == 0)        
-%             if(c == columns)
-%                 ycbcrReconstructed(r,c,Cb) = ycbcrReconstructed(r,c-1,Cb);
-%                 ycbcrReconstructed(r,c,Cr) = ycbcrReconstructed(r,c-1,Cr);
-%             else
-%                 ycbcrReconstructed(r,c,Cb) = (ycbcrReconstructed(r,c-1,Cb) + ycbcrReconstructed(r,c+1,Cb))/2;
-%                 ycbcrReconstructed(r,c,Cr) = (ycbcrReconstructed(r,c-1,Cr) + ycbcrReconstructed(r,c+1,Cr))/2;
-%             end
-%         end
-%     end
-% end
+% % 6.1. Linear interpolation %
 % 
-% for r=1:rows
-%     for c=1:columns
-%         if(mod(r,2) == 0)
-%             if(r == rows)
-%                 ycbcrReconstructed(r,c,Cb) = ycbcrReconstructed(r-1,c,Cb);
-%                 ycbcrReconstructed(r,c,Cr) = ycbcrReconstructed(r-1,c,Cr);
-%             else
-%                 ycbcrReconstructed(r,c,Cb) = (ycbcrReconstructed(r-1,c,Cb) + ycbcrReconstructed(r+1,c,Cb))/2;
-%                 ycbcrReconstructed(r,c,Cr) = (ycbcrReconstructed(r-1,c,Cr) + ycbcrReconstructed(r+1,c,Cr))/2;
-%             end
-%         end
-%     end
-% end
-
-
-figure, imshow(ycbcrReconstructed); title('Reconstructed'); %Just to see what Im getting
-
-figure, imshow(ycbcr); title('Original'); %Original
+% % For odd row, compute the missing pixel as an average of the
+% % pixels in its right and left sides.
+% 
+% % For even rows, compute the missing pixel as an average of the
+% % pixels in its top and bottom sides.
+% 
+% % [SubsampledRow, SubsampledColumn, SubsampledChannels] = size(ycbcrSubsampled);
+% ycbcrReconstructed = luma(:,:,Y);
+% 
+% % rCount = 1;
+% % cCount = 1;
+% 
+% %Copying all pixels from ycbcrSubsampled into ycbcrReconstructed at every
+% %odd location
+% 
+% %work in progress
+% 
+% ycbcrReconstructed(1:2:end,1:2:end,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
+% % ycbcrReconstructed(:,:,Cb:Cr) = ycbcrSubsampled(:,:,Cb:Cr);
+% 
+% oddPixelRows = (ycbcrSubsampled(:,1:1:end-1,Cb:Cr) + ycbcrSubsampled(:,2:1:end,Cb:Cr))/2;
+% 
+% % if(mod(columns,2) == 0)
+% %     oddPixelRows(:,end,Cb-1:Cr-1) = oddPixelRows(:,end-1,Cb-1:Cr-1);
+% % end
+% 
+% evenPixelRows = (ycbcrSubsampled(1:1:end-1,:,Cb:Cr) + ycbcrSubsampled(2:1:end,:,Cb:Cr))/2;
+% 
+% % if(mod(rows,2) == 0)
+% %     evenPixelRows (end,:,Cb-1:Cr-1) = evenPixelRows (end-1,:,Cb-1:Cr-1);
+% % end
+% 
+% ycbcrReconstructed(1:2:end,2:2:end-1,Cb:Cr) = oddPixelRows(:,:,Cb-1:Cr-1);
+% % ycbcrReconstructed(:,:,Cb:Cr) = oddPixelRows(:,:,Cb:Cr);
+% ycbcrReconstructed(2:2:end-1,:,Cb:Cr) = (ycbcrReconstructed(1:2:end-2,:,Cb:Cr) + ycbcrReconstructed(3:2:end,:,Cb:Cr))/2;
+% 
+% 
+% 
+% % for r=1:rows
+% %     for c=1:columns          %this works but Im working on something simpler above
+% %         if(mod(c,2) == 0)        
+% %             if(c == columns)
+% %                 ycbcrReconstructed(r,c,Cb) = ycbcrReconstructed(r,c-1,Cb);
+% %                 ycbcrReconstructed(r,c,Cr) = ycbcrReconstructed(r,c-1,Cr);
+% %             else
+% %                 ycbcrReconstructed(r,c,Cb) = (ycbcrReconstructed(r,c-1,Cb) + ycbcrReconstructed(r,c+1,Cb))/2;
+% %                 ycbcrReconstructed(r,c,Cr) = (ycbcrReconstructed(r,c-1,Cr) + ycbcrReconstructed(r,c+1,Cr))/2;
+% %             end
+% %         end
+% %     end
+% % end
+% % 
+% % for r=1:rows
+% %     for c=1:columns
+% %         if(mod(r,2) == 0)
+% %             if(r == rows)
+% %                 ycbcrReconstructed(r,c,Cb) = ycbcrReconstructed(r-1,c,Cb);
+% %                 ycbcrReconstructed(r,c,Cr) = ycbcrReconstructed(r-1,c,Cr);
+% %             else
+% %                 ycbcrReconstructed(r,c,Cb) = (ycbcrReconstructed(r-1,c,Cb) + ycbcrReconstructed(r+1,c,Cb))/2;
+% %                 ycbcrReconstructed(r,c,Cr) = (ycbcrReconstructed(r-1,c,Cr) + ycbcrReconstructed(r+1,c,Cr))/2;
+% %             end
+% %         end
+% %     end
+% % end
+% 
+% 
+% figure, imshow(ycbcrReconstructed); title('Reconstructed'); %Just to see what Im getting
+% 
+% figure, imshow(ycbcr); title('Original'); %Original
 
 % 6.2. Simple row or column replication. %
+% using ycbcrSubsampled
 
+ycbcrReconstructed62 = ycbcrSubsampled; % init just to ensure dimensions
+% figure, imshow(ycbcrReconstructed62); title('[6.2] Before reconstruction');
 
+for r = 1:rows
+    for c = 1:columns
+        % if we are in an odd row and it's an even column
+        if ((mod(c, 2) == 0) && mod(r, 2) ~= 0)
+            % copying the value from the column before index
+            ycbcrReconstructed62(r,c,Cr) = ycbcrReconstructed62(r,c-1,Cr);
+            ycbcrReconstructed62(r,c,Cb) = ycbcrReconstructed62(r,c-1,Cb);
+        % if it is an even row
+        elseif (mod(r, 2) == 0)
+            % copies the entire previous row
+            ycbcrReconstructed62(r,:,Cr) = ycbcrReconstructed62(r-1,:,Cr);
+            ycbcrReconstructed62(r,:,Cb) = ycbcrReconstructed62(r-1,:,Cb);
+        end
+    end
+end
+
+ycbcrReconstructed62=ycbcr2rgb(ycbcrReconstructed62); % convert back to RGB
+% figure, imshow(ycbcrReconstructed62); title('[6.2] Reconstructed');
+% figure, imshow(rgbImage); title('[6.2] Original');
 
 %% 7. Convert the image into RGB format. (5 points) %%
